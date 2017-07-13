e735ddfa0cdac2017b080c969a85b4578c8e6b65
Yiwen Gong <gongyiwen2017@gmail.com>
2017-07-13 16:35:05 -0400

    Setup start and heroku port

--- server.js
+++ server.js
@@ -2,20 +2,13 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-hbs.registerHelper('getCurrentYear', ()=>{
-  return new Date().getFullYear();
-});
-
-hbs.registerHelper('screamIt', (text)=>{
-  return text.toUpperCase();
-});
-
-app.use((req,res,next) =>{
+app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
 
@@ -34,17 +27,34 @@ app.use((req,res,next) =>{
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req,res) => {
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle:'Home Page',
-    welcomeMessage:'Welcome to my website'
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
-app.get('/about', (req,res) =>{
+app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle:'About Page',
+    pageTitle: 'About Page'
   });
 });
 
-app.listen(3000);
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
